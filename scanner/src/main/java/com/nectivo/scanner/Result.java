de74bfeea6fee35ea85156c1e23f3080cb9fea92
Piotr Babel <piotr.babel@gmail.com>
2014-03-29 18:37:30 +0100

    Model passed after success scanning.

---
+++ scanner/src/main/java/com/nectivo/scanner/Result.java
@@ -0,0 +1,25 @@
+package com.nectivo.scanner;
+
+/**
+ * Created by piobab on 29.03.2014.
+ */
+public class Result {
+    private int mCodeType;
+    private String mContent;
+
+    public int getCodeType() {
+        return mCodeType;
+    }
+
+    public void setCodeType(int codeType) {
+        this.mCodeType = mCodeType;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        this.mContent = mContent;
+    }
+}
